f5f29ed1cabb9214531662c2d39176094ef8fddd
Lucas <l.maloschek@gmail.com>
2022-12-20 06:22:34 +0100

    Delete cache.bat

--- cache.bat
+++
@@ -1,8 +0,0 @@
-pushd "%LocalAppData%\FiveM\FiveM.app\data" || exit /B 1
-for /D %%D in ("*") do (
-  if /I not "%%~nxD"=="game-storage" rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-  del "%%~F"
-)
-popd
